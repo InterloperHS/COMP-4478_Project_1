af8a01ce3fb57035535b9d3d34bdf2d2ebbe5769
tahmidul612 <tahmidul612@gmail.com>
2023-01-31 21:34:35 -0500

    menu with play and options buttons

---
+++ source/MenuState.hx
@@ -0,0 +1,32 @@
+package;
+
+import flixel.util.FlxColor;
+import flixel.text.FlxText;
+import flixel.FlxG;
+import flixel.ui.FlxButton;
+import flixel.FlxState;
+
+class MenuState extends FlxState
+{
+    var playButton:FlxButton;
+    var optionsButton:FlxButton;
+    override public function create() {
+        super.create();
+
+        playButton = new FlxButton(0, 0, "Play", clickPlay);
+        playButton.x = (FlxG.width / 2) - (playButton.width/2);
+        playButton.y = (FlxG.height/2) - (playButton.height/2) - 10;
+        add(playButton);
+       
+        optionsButton = new FlxButton(0, 0, "Options", clickOptions);
+        optionsButton.x = (FlxG.width / 2) - (optionsButton.width/2);
+        optionsButton.y = (FlxG.height/2) - (optionsButton.height/2) + 10;
+        add(optionsButton);
+    }
+    function clickPlay() {
+        FlxG.switchState(new PlayState());
+    }
+    function clickOptions() {
+        FlxG.switchState(new OptionsState());
+    }
+}
