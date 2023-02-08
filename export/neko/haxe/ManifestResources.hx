f99fd54332b321f60b4db2185abe1f6733379c78
Austin Hart <arhart1@lakeheadu.ca>
2023-02-08 16:08:04 -0500

    Update player and enemy health

--- export/neko/haxe/ManifestResources.hx
+++ export/neko/haxe/ManifestResources.hx
@@ -105,7 +105,9 @@ null
 #if flash
 
 @:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_data_data_goes_here_txt extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_images_crosshair_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
 @:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_images_images_go_here_txt extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_images_walking_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
 @:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_music_music_goes_here_txt extends null { }
 @:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_sounds_sounds_go_here_txt extends null { }
 @:keep @:bind @:noCompletion #if display private #end class __ASSET__flixel_sounds_beep_ogg extends null { }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,5 +1,7 @@
 package;
 
+import flixel.util.FlxTimer;
+import flixel.ui.FlxBar;
 import flixel.FlxState;
 import flixel.FlxSprite;
 import flixel.util.FlxSpriteUtil;
@@ -9,26 +11,45 @@ import flixel.FlxG;
 import flixel.group.FlxGroup;
 import flixel.FlxObject;
 import flixel.math.FlxRandom;
+import flixel.math.FlxAngle;
+import flixel.text.FlxText;
+import flixel.effects.FlxFlicker;
 
 class PlayState extends FlxState
 {
-	var player:FlxSprite;
+	//var player:FlxSprite;
 	var bullets:FlxTypedGroup<FlxSprite>;
 	var enemies:FlxTypedGroup<FlxSprite>;
 	var enemy:FlxSprite;
 	var random:FlxRandom;
+	var ammoNum:Int;
+	var ammoBoxes:FlxTypedGroup<FlxSprite>;
+	var ammoBox:FlxSprite;
+	var player:Player;
+	var ammoText:FlxText;
+	var pHealthBar:FlxBar;
+	var enemyHealthBars:FlxTypedGroup<FlxBar>;
+	var enemyHealth:FlxBar;
+	var grpWalls:FlxTypedGroup<FlxSprite> = null;
+	var timer:FlxTimer;
 	override public function create()
 	{
-		super.create();
+		FlxG.mouse.load("assets/images/crosshair.png", 0.12, -18, -18);
+		
 		//Generate a new random key
 		random = new FlxRandom();
 
+		timer = new FlxTimer();
+
 		//Create the player and add them to the screen
-		player = new FlxSprite(FlxG.width/2, FlxG.height/2);
-		player.makeGraphic(20,20, FlxColor.WHITE);
+		player = new Player(FlxG.width/2, FlxG.height/2);
+		player.health = 100;
 		add(player);
 
-		//Create the bullets group
+		
+
+		//Create the bullets group and set ammo
+		ammoNum = 20;
 		var numBullets:Int = 10;
 		bullets = new FlxTypedGroup(numBullets);
 
@@ -43,19 +64,50 @@ class PlayState extends FlxState
 		add(bullets);
 
 		//Create 10 enemies off screen
+		enemyHealthBars = new FlxTypedGroup(10);
 		enemies = new FlxTypedGroup(10);
 		for(i in 0...10){
 			enemy = new FlxSprite(-200,-200);
 			enemy.makeGraphic(10,10, FlxColor.RED);
 			enemy.exists = false;
+			enemy.health = 100;
+
+			enemyHealth = new FlxBar(enemy.x,enemy.y,LEFT_TO_RIGHT,100,10,enemy,"health", 0, 100, false);
+			enemyHealth.exists = false;
+			enemyHealth.killOnEmpty = true;
+
 			enemies.add(enemy);
+			enemyHealthBars.add(enemyHealth);
+
 		}
 		add(enemies);
+		add(enemyHealthBars);
+
+		//Create 10 ammo boxes off screen
+		ammoBoxes = new FlxTypedGroup(10);
+		for(i in 0...10){
+			ammoBox = new FlxSprite(-200,-200);
+			ammoBox.makeGraphic(10,10, FlxColor.BLUE);
+			ammoBox.exists = false;
+			ammoBoxes.add(ammoBox);
+		}
+		add(ammoBoxes);
+
+		//Ammo display
+		ammoText = new FlxText(0,FlxG.height-24,FlxG.width, "Ammo: " + ammoNum, 16);
+		ammoText.setFormat(null,16,FlxColor.WHITE,FlxTextAlign.LEFT);
+		add(ammoText);
+
+		pHealthBar = new FlxBar(FlxG.width - 210, FlxG.height -24,LEFT_TO_RIGHT,200,20,player,"health", 0, 100, false);
+		add(pHealthBar);
+
+		super.create();
 	}
 
-	var angle:Int;
+	var angle:Float;
 	var xDist:Float;
 	var yDist:Float;
+
 	override public function update(elapsed:Float)
 	{	
 		//Calculate y diff and x diff of the mouse and player
@@ -63,11 +115,13 @@ class PlayState extends FlxState
 		xDist = FlxG.mouse.x - player.x;
 		//Calculate the cotangent to find the angle of the mouse relative to the player
 		//and then convert it from radians to degrees.
-		angle = Std.int(Math.atan(yDist/xDist) * 57.2957795);
-		player.angle = angle;
+		angle = FlxAngle.angleBetweenMouse(player, true);
 
-		if(FlxG.keys.justPressed.SPACE){
+		if(FlxG.mouse.justPressed && ammoNum > 0){
 			//Create a new bullet at the player and point it the same angle of the player
+			ammoNum--;
+			
+			
 			var bullet:FlxSprite = bullets.recycle();
 			bullet.x = player.x;
 			bullet.y = player.y;
@@ -81,31 +135,36 @@ class PlayState extends FlxState
 			bullet.velocity.y =(yDist / dist) * speed;
 			bullet.velocity.x =(xDist / dist) * speed;
 		}
-
-		//Controls player movement and sets the bounds
-		if(FlxG.keys.pressed.A && player.x > 0){
-			player.x-=2;
-		}
-		if(FlxG.keys.pressed.D && player.x < (FlxG.width - player.width)){
-			player.x+=2;
-		}
-		if(FlxG.keys.pressed.W && player.y > 0){
-			player.y-=2;
-		}
-		if(FlxG.keys.pressed.S && player.y < (FlxG.height - player.height)){
-			player.y+=2;
-		}
-
+		
 		//Check if the bullets are out of bounds or touching an enemy
 		bullets.forEachAlive(outOfBounds);
 		FlxG.overlap(bullets, enemies, killEnemies);
-
+		//Check if player is touching an ammo box
+		FlxG.overlap(ammoBoxes, player, addAmmo);
+		
+		FlxG.overlap(player, enemies, hurtPlayer);
+		
 		//Respawn a testing enemy
 		if(enemies.countLiving() < 1){
 			var enemy:FlxSprite = enemies.recycle();
+			var eHealth:FlxBar = enemyHealthBars.recycle();
+
 			enemy.x = random.int(10,FlxG.width);
 			enemy.y = random.int(10,FlxG.height);
+
+			//Set the enemy health bar to follow the enemy we just spawned
+			eHealth.setParent(enemy, "health", true, Std.int(eHealth.width/2 - enemy.width/2) * -1 , -12);
+			trace(enemy.getPosition());
 		}
+		
+		//Respawn an ammo box
+		if(ammoBoxes.countLiving() < 1){
+			var ammo:FlxSprite = ammoBoxes.recycle();
+			ammo.x = random.int(10,FlxG.width);
+			ammo.y = random.int(10,FlxG.height);
+		}
+		//Update the ammo display text
+		ammoText.text = "Ammo: " + Std.string(ammoNum);
 
 		super.update(elapsed);
 	}
@@ -121,6 +180,32 @@ class PlayState extends FlxState
 	public function killEnemies(bullet:FlxObject, e:FlxSprite){
 		//If a bullet hits an enemy, kill the bullet and enemy
 		bullet.kill();
-		e.kill();
+		trace(e.health);
+		
+		//
+		e.health-=25;
+		if(e.health<=0){
+			e.kill();
+		}
+	}
+
+	public function hurtPlayer(p:Player, e:FlxObject){
+		trace(p.health);
+		FlxG.camera.flash(FlxColor.WHITE,1);
+		
+		FlxG.collide(p,e);
+		if(!FlxFlicker.isFlickering(p)){
+			p.health -=25;
+			if(p.health<=0){
+				p.kill();
+			}
+		}
+		FlxFlicker.flicker(p); 
+	}
+
+	public function addAmmo(ammo:FlxObject, p:FlxSprite){
+		//If the player touches an ammo box, kill it and increase their ammo by 5
+		ammo.kill();
+		ammoNum+=5;
 	}
 }
