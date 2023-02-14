68cddd22b95a41918c3b3bdbcb65413f1ad20715
tahmidul612 <tahmidul612@gmail.com>
2023-02-14 16:28:11 -0500

    added enemy type parameter, enemy assets+animation

--- source/Enemy.hx
+++ source/Enemy.hx
@@ -1,5 +1,6 @@
 package;
 
+import flixel.math.FlxAngle;
 import flixel.util.FlxColor;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.FlxSprite;
@@ -10,7 +11,9 @@ class Enemy extends FlxSprite {
 	var numBullets:Int = 5;
 	var bullets:FlxTypedGroup<FlxSprite>;
 
-	public function new(x:Float = 0, y:Float = 0, play:Player, spd:Float) {
+	public function new(x:Float = 0, y:Float = 0, play:Player, spd:Float, enemyType:String) {
+		super(x, y);
+
 		// The enemy wants to get the player
 		target = play;
 		speed = spd;
@@ -27,7 +30,51 @@ class Enemy extends FlxSprite {
 			}
 		}
 
-		super(x, y);
+		// Set the enemy's graphic based on the type
+		switch (enemyType) {
+			default:
+			case "normal":
+				loadGraphic(AssetPaths.zombie__png, true, 16, 16);
+				setGraphicSize(16, 16);
+				setSize(8, 8);
+				offset.x = 4;
+				offset.y = 4;
+			case "ranged":
+				loadGraphic(AssetPaths.zombie_f__png, true, 16, 16);
+				setGraphicSize(16, 16);
+				setSize(8, 8);
+				offset.x = 4;
+				offset.y = 4;
+			case "large":
+				loadGraphic(AssetPaths.zombie__png, true, 16, 16);
+				setGraphicSize(32, 32);
+				setSize(16, 16);
+				offset.x = 8;
+				offset.y = 8;
+		}
+		createAnimations();
+	}
+
+	private function createAnimations() {
+		// Create the walk animations
+		animation.add("walkD", [0, 1, 2, 3], 5, true);
+		animation.add("walkDL", [4, 5, 6, 7], 5, true);
+		animation.add("walkL", [8, 9, 10, 11], 5, true);
+		animation.add("walkUL", [12, 13, 14, 15], 5, true);
+		animation.add("walkU", [16, 17, 18, 19], 5, true);
+		animation.add("walkDR", [20, 21, 22, 23], 5, true);
+		animation.add("walkR", [24, 25, 26, 27], 5, true);
+		animation.add("walkUR", [28, 29, 30, 31], 5, true);
+
+		// Create the idle animations
+		animation.add("idleD", [0], 5, true);
+		animation.add("idleDL", [4], 5, true);
+		animation.add("idleL", [8], 5, true);
+		animation.add("idleUL", [12], 5, true);
+		animation.add("idleU", [16], 5, true);
+		animation.add("idleDR", [20], 5, true);
+		animation.add("idleR", [24], 5, true);
+		animation.add("idleUR", [28], 5, true);
 	}
 
 	override public function update(elapsed:Float):Void {
