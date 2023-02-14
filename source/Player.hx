0737bd4847a065f7a44360d87f401e7d4f559daf
tahmidul612 <tahmidul612@gmail.com>
2023-02-14 16:28:51 -0500

    added asset+animation

--- source/Player.hx
+++ source/Player.hx
@@ -8,30 +8,28 @@ import flixel.math.FlxAngle;
 class Player extends FlxSprite {
 	var healthBar:FlxBar;
 
-	static inline var SPEED:Float = 100;
-
 	public function new(x:Float = 0, y:Float = 0) {
-		// Create a constructor for the Player class
 		super(x, y);
-
-		// Create the sprite and load the spritesheet
-		var spritesheet = new FlxSprite();
-		loadGraphic(AssetPaths.walking__png, true, 16, 16);
-
+		// Create a constructor for the Player class
+		loadGraphic(AssetPaths.player__png, true, 16, 16);
+		createAnimations();
+		setGraphicSize(16, 16);
 		setSize(8, 8);
 		offset.x = 4;
 		offset.y = 4;
 		drag.x = drag.y = 800;
+	}
 
+	private function createAnimations() {
 		// Create the walk animations
 		animation.add("walkD", [0, 1, 2, 3], 5, true);
 		animation.add("walkDL", [4, 5, 6, 7], 5, true);
 		animation.add("walkL", [8, 9, 10, 11], 5, true);
 		animation.add("walkUL", [12, 13, 14, 15], 5, true);
 		animation.add("walkU", [16, 17, 18, 19], 5, true);
-		animation.add("walkUR", [20, 21, 22, 23], 5, true);
+		animation.add("walkDR", [20, 21, 22, 23], 5, true);
 		animation.add("walkR", [24, 25, 26, 27], 5, true);
-		animation.add("walkDR", [28, 29, 30, 31], 5, true);
+		animation.add("walkUR", [28, 29, 30, 31], 5, true);
 
 		// Create the idle animations
 		animation.add("idleD", [0], 5, true);
@@ -39,12 +37,12 @@ class Player extends FlxSprite {
 		animation.add("idleL", [8], 5, true);
 		animation.add("idleUL", [12], 5, true);
 		animation.add("idleU", [16], 5, true);
-		animation.add("idleUR", [20], 5, true);
+		animation.add("idleDR", [20], 5, true);
 		animation.add("idleR", [24], 5, true);
-		animation.add("idleDR", [28], 5, true);
+		animation.add("idleUR", [28], 5, true);
 	}
 
-	function updateMovement() {
+	private function updateMovement() {
 		// Set variables that store which direction the player wants to go based on keys pressed
 		var up:Bool = FlxG.keys.anyPressed([W, UP]);
 		var down:Bool = FlxG.keys.anyPressed([S, DOWN]);
@@ -53,34 +51,29 @@ class Player extends FlxSprite {
 
 		// Calculate the angle and set a default direction
 		var mouseAngle = FlxAngle.angleBetweenMouse(this, true);
-		var direction:String = "D";
+		var pointerDirection:String = "D";
 
 		// Set direction based on orientation from player to mouse
 		if (mouseAngle > -157.5 && mouseAngle <= -112.5) {
-			direction = "UL";
+			pointerDirection = "UL";
 		} else if (mouseAngle > -112.5 && mouseAngle <= -67.5) {
-			direction = "U";
+			pointerDirection = "U";
 		} else if (mouseAngle > -67.5 && mouseAngle <= -22.5) {
-			direction = "UR";
+			pointerDirection = "UR";
 		} else if (mouseAngle > -22.5 && mouseAngle <= 22.5) {
-			direction = "R";
+			pointerDirection = "R";
 		} else if (mouseAngle > 22.5 && mouseAngle <= 67.5) {
-			direction = "DR";
+			pointerDirection = "DR";
 		} else if (mouseAngle > 67.5 && mouseAngle <= 112.5) {
-			direction = "D";
+			pointerDirection = "D";
 		} else if (mouseAngle > 112.5 && mouseAngle <= 157.5) {
-			direction = "DL";
+			pointerDirection = "DL";
 		} else if ((mouseAngle > 157.5 && mouseAngle <= 180) || (mouseAngle > -180 && mouseAngle <= -157.5)) {
-			direction = "L";
+			pointerDirection = "L";
 		}
 
-		// Set the animation to idle by default, change to walk if the player is moving
-		var action = "idle";
-		if (velocity.x != 0 || velocity.y != 0) {
-			action = "walk";
-		}
-		animation.play(action + direction);
-
+		// Variable to store the direction the play is moving
+		var moveDirection:String = "D";
 		// If one of the keys are pressed
 		if (up || down || left || right) {
 			// Check if keys cancel each other out
@@ -97,31 +90,52 @@ class Player extends FlxSprite {
 			var angle:Float = 0;
 			if (up) {
 				angle = -90;
+				moveDirection = "U";
 				if (left) {
 					angle -= 45;
+					moveDirection = "UL";
 				} else if (right) {
 					angle += 45;
+					moveDirection = "UR";
 				}
 			} else if (down) {
 				angle = 90;
+				moveDirection = "D";
 				if (left) {
 					angle += 45;
+					moveDirection = "DL";
 				} else if (right) {
 					angle -= 45;
+					moveDirection = "DR";
 				}
 			} else if (left) {
 				angle = 180;
+				moveDirection = "L";
 			} else if (right) {
 				angle = 0;
+				moveDirection = "R";
 			}
 
 			// Set velocity relative to the SPEED var and angle we just calculated
-			velocity.setPolarDegrees(SPEED, angle);
+			velocity.setPolarDegrees(Reg.SPEED * 10, angle);
+		}
+
+		// Set the animation to idle by default, change to walk if the player is moving
+		var action = "idle";
+		if ((velocity.x != 0 || velocity.y != 0) && touching == NONE) {
+			action = "walk";
+		}
+		if (action == "walk") {
+			// Play the walk animation
+			animation.play(action + moveDirection);
+		} else {
+			// Play the idle animation
+			animation.play(action + pointerDirection);
 		}
 	}
 
-	override function update(elapsed:Float) {
-		updateMovement();
+	override public function update(elapsed:Float) {
 		super.update(elapsed);
+		updateMovement();
 	}
 }
