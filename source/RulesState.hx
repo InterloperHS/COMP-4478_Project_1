e472cb9790ccabbacbea8baaf3c09e0d4693f8d1
tahmidul612 <tahmidul612@gmail.com>
2023-02-15 19:28:51 -0500

    menu with rules

---
+++ source/RulesState.hx
@@ -0,0 +1,28 @@
+package;
+
+import flixel.util.FlxColor;
+import flixel.FlxG;
+import flixel.addons.ui.FlxUISubState;
+
+class RulesState extends FlxUISubState {
+	override public function new(BGColor:FlxColor) {
+		super(BGColor);
+	}
+
+	override public function create() {
+		FlxG.camera.zoom = 1;
+		_xml_id = "rules_ui";
+		super.create();
+	}
+
+	override public function getEvent(event:String, target:Dynamic, data:Dynamic, ?params:Array<Dynamic>):Void {
+		if (params != null) {
+			switch (event) {
+				case "click_button":
+					switch (Std.string(params[0])) {
+						case "back": close();
+					}
+			}
+		}
+	}
+}
